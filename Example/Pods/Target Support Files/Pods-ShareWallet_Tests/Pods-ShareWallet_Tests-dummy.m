144cb403edc2feeaf6423f861651dc0049afeeaa
ashahrouj <shahrouj@peakperformances.io>
2023-02-23 15:06:07 +0400

    first commit

---
+++ Example/Pods/Target Support Files/Pods-ShareWallet_Tests/Pods-ShareWallet_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ShareWallet_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ShareWallet_Tests
+@end
---
+++ Example/Pods/Target Support Files/ShareWallet/ShareWallet-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ShareWallet : NSObject
+@end
+@implementation PodsDummy_ShareWallet
+@end
